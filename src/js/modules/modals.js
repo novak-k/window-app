791b3856df01062b74ca5261ae718b0131a7e432
Kate Novak <novak5683@gmail.com>
2021-07-05 20:16:12 +0200

    screenshots

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,7 +4,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector), 
              modal = document.querySelector(modalSelector),
              close = document.querySelector(closeSelector),
-             windows = document.querySelectorAll('[data-modal]');
+             windows = document.querySelectorAll('[data-modal]'),
+             scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
 
                     modal.style.display = 'block';
                     document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scroll}px`;
             });    
         });
 
@@ -38,6 +40,7 @@ const modals = () => {
 
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
             // document.body.classList.remove('modal-open');
         });
 
@@ -48,6 +51,7 @@ const modals = () => {
                 });
                 modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
             // document.body.classList.remove('modal-open');
             }
         });
@@ -64,6 +68,25 @@ const modals = () => {
             document.body.style.overflow = 'hidden';
         }, time);
     }  
+
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px'; 
+        div.style.height = '50px'; 
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+
+    }
+
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
